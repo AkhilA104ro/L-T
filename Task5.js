0697ea8e7c9b5ee547964932e4c3b5fba560cf73
AkhilA104ro <akhil.mohan@btech.christuniversity.in>
2026-07-23 23:07:25 +0530

    Create Task5.js

---
+++ Task5.js
@@ -0,0 +1,13 @@
+// ===================================
+let age = 65;
+
+console.log("Task 5");
+if (age >= 60) {
+    console.log("Patient is a Senior Citizen.");
+} else {
+    console.log("Patient is not a Senior Citizen.");
+}
+console.log();
+
+
+// ===================================
