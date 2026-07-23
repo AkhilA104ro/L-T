9f3131def12b7fb89aedffa1ecd9e158c0d963bd
AkhilA104ro <akhil.mohan@btech.christuniversity.in>
2026-07-23 23:04:40 +0530

    Create Task1.js

---
+++ Task1.js
@@ -0,0 +1,6 @@
+// =======================
+console.log("===== Hospital Patient System =====");
+console.log("Welcome to the Hospital Patient System!\n");
+
+
+// ======================================
